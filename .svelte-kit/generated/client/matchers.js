1dee042985da3a71c0535a3bc26970e09a4f3639
Matthew Blott <m4ttb@pm.me>
2026-04-10 13:06:51 +0100

    Removed .svelte-kit from repo as it shouldn't be included.

--- .svelte-kit/generated/client/matchers.js
+++
@@ -1,3 +0,0 @@
-import { match as username } from "../../../src/params/username.ts";
-
-export const matchers = { username };
--- .svelte-kit/types/src/routes/(public)/proxy+layout.server.ts
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-// src/routes/(public)/+layout.server.ts
-import type { LayoutServerLoad } from './$types';
-
-export const load = async ({ locals }: Parameters<LayoutServerLoad>[0]) => {
-  return { user: locals.user ?? null };
-};
